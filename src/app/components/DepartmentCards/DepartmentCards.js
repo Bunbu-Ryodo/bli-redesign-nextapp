98df0a2f30083c546a229938bc6d093a02944282
Jay Lacey <jaylacey22@pm.me>
2023-07-03 20:52:10 +0100

    Finished home page, and fixed broken Karate Department route

--- src/app/components/DepartmentCards/DepartmentCards.js
+++ src/app/components/DepartmentCards/DepartmentCards.js
@@ -9,23 +9,32 @@ import bjjHover from './bjj_hover.png';
 import kickboxing from './kickboxing.png';
 import kickboxingHover from './kickboxing_hover.png';
 
+import { Dosis } from 'next/font/google';
+const dosis = Dosis({subsets: ['latin'], weight: ['400']});
+
+
 function DepartmentCards(){
     return (
-        <div class="container flex flex-col md:flex-row justify-items-evenly px-6 mx-auto mt-10 space-x-2 space-y-0 md:space-y-0">
-            <a className="flex flex-col md:w-1/2" href="/karatedepartment">
-                <Image src={karateHover} width={317} height={420} alt="Karate"/>
-            </a>
-            <div className="flex flex-col md:w-1/2">
-                <Image src={judoHover} width={317} height={420} alt="Judo"/>
+        <section id="home">
+            <div className="container flex flex-row mx-auto justify-items-start">
+                <a href="/" className={dosis.className + " flex flex-col justify-self-auto px-2 text-blitzBlue hover:text-blitzRed"}>Home</a>
             </div>
-            <div className="flex flex-col md:w-1/2">
-                <Image src={bjjHover} width={317} height={420} alt="BJJ"/>
+            <div className="container flex flex-col md:flex-row justify-items-evenly px-6 mx-auto mt-10 space-x-2 space-y-0 md:space-y-0">
+                <a className="flex flex-col md:w-1/2" href="/karatedepartment">
+                    <Image src={karateHover} width={317} height={420} alt="Karate"/>
+                </a>
+                <div className="flex flex-col md:w-1/2">
+                    <Image src={judoHover} width={317} height={420} alt="Judo"/>
+                </div>
+                <div className="flex flex-col md:w-1/2">
+                    <Image src={bjjHover} width={317} height={420} alt="BJJ"/>
+                </div>
+                <div className="flex flex-col md:w-1/2">
+                    <Image src={kickboxingHover} width={317} height={420} alt="Kickboxing"/>
+                </div>
             </div>
-            <div className="flex flex-col md:w-1/2">
-                <Image src={kickboxingHover} width={317} height={420} alt="Kickboxing"/>
-            </div>
-        
-        </div>
+        </section>
+   
     );
 }
 
--- src/app/karatedepartment/page.js
+++ src/app/karatedepartment/page.js
@@ -1,3 +1,14 @@
-export default function KarateDepartment(){
-   return(<h1>TEST TEST TEST</h1>);
+import Navbar from '../components/Navbar/Navbar';
+
+export default function KarateDepartment() {
+  return (
+    <main>
+      <div className="flex-row">
+        <Navbar></Navbar>
+        <section id="karatedepartment">
+            <h1>Karate Department Goes Here</h1>
+        </section>
+      </div>
+    </main>
+  )
 }
