e5aabfd69e95ec076d00ca2be8ef3c378aaef9b9
Jay Lacey <jaylacey22@pm.me>
2023-07-03 21:09:29 +0100

    Collage scales nicely

--- src/app/components/DepartmentCards/DepartmentCards.js
+++ src/app/components/DepartmentCards/DepartmentCards.js
@@ -17,7 +17,7 @@ function DepartmentCards(){
     return (
         <section id="home">
             <div className="container flex flex-row mx-auto justify-items-start">
-                <a href="/" className={dosis.className + " flex flex-col justify-self-auto px-2 text-blitzBlue hover:text-blitzRed"}>Home</a>
+                <a href="/" className={dosis.className + " flex flex-col justify-self-auto px-1 text-blitzBlue hover:text-blitzRed"}>Home</a>
             </div>
             <div className="container flex flex-col md:flex-row justify-items-evenly px-6 mx-auto mt-10 space-x-2 space-y-0 md:space-y-0">
                 <a className="flex flex-col md:w-1/2" href="/karatedepartment">
--- src/app/karatedepartment/page.js
+++ src/app/karatedepartment/page.js
@@ -1,4 +1,9 @@
+import Image from 'next/image';
 import Navbar from '../components/Navbar/Navbar';
+import karateDepartmentHome from './karate_department_home.png';
+
+import { Dosis } from 'next/font/google';
+const dosis = Dosis({subsets: ['latin'], weight: ['400']});
 
 export default function KarateDepartment() {
   return (
@@ -6,7 +11,13 @@ export default function KarateDepartment() {
       <div className="flex-row">
         <Navbar></Navbar>
         <section id="karatedepartment">
-            <h1>Karate Department Goes Ha</h1>
+         <div className="container flex flex-row mx-auto justify-items-start">
+            <a href="/" className={dosis.className + " flex flex-col justify-self-auto px-1 text-blitzBlue hover:text-blitzRed"}>Home /</a>
+            <a href="#" className={dosis.className + " flex flex-col justify-self-auto px-1 text-blitzRed"}>Karate Department</a>
+         </div>
+         <div className="container flex flex-row mx-auto items-center">
+            <Image src={karateDepartmentHome}/>
+         </div>
         </section>
       </div>
     </main>
