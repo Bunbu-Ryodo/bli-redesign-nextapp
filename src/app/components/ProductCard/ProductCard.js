6e9374b6bd75e329c28711b990ab70cc69e83d29
Jay Lacey <jaylacey22@pm.me>
2023-07-05 12:44:02 +0100

    Finished Card with Props

--- src/app/components/ProductCard/ProductCard.js
+++ src/app/components/ProductCard/ProductCard.js
@@ -2,17 +2,17 @@ import Image from 'next/image';
 import diamond from './diamond.jpg';
 import { bebasNeue, dosisSemibold } from '../../fonts';
 
-export default function ProductCard(){
+export default function ProductCard({link, image, alt, productDescription, price, rating, reviews}){
     return(
         <div 
          class="flex flex-col px-4 py-4 items-center border border-blitzBlack hover:border-none hover:shadow-xl w-80 h-128">
-            <a href="/karatedepartment">
+            <a href={link}>
             <div className="flex flex-row justify-center mb-10">
-                <Image src={diamond} alt="diamond"></Image>
+                <Image src={diamond} alt={alt}></Image>
             </div>
-            <h3 className={bebasNeue.className + " text-blitzBlack"}>Blitz Adult Diamond Kata Karategi</h3>
-            <h3 className={bebasNeue.className + " text-blitzRed"}>£95.99 - £115.99</h3>
-            <h3 className={bebasNeue.className + " text-blitzYellow"}>5/5 <span className="text-blitzBlue">(75 Reviews)</span></h3>
+            <h3 className={bebasNeue.className + " text-blitzBlack"}>{productDescription}</h3>
+            <h3 className={bebasNeue.className + " text-blitzRed"}>{price}</h3>
+            <h3 className={bebasNeue.className + " text-blitzYellow"}>{rating} <span className="text-blitzBlue">{reviews}</span></h3>
             </a>
         </div>
     )
--- src/app/karateuniforms/page.js
+++ src/app/karateuniforms/page.js
@@ -1,12 +1,21 @@
 import Navbar from '../components/Navbar/Navbar';
 import ProductCard from '../components/ProductCard/ProductCard';
+import Diamond from './diamond.jpg';
 
 export default function KarateUniforms() {
   return (
     <main>
       <div className="container mx-auto">
         <Navbar></Navbar>
-        <ProductCard/>
+        <ProductCard 
+          link="/"
+          image={Diamond} 
+          alt="Diamond"
+          productDescription="Blitz Diamond Kata Karate Gi" 
+          price="£95.99-£115.99" 
+          rating="5/5" 
+          reviews="(75 Reviews)"
+        />
       </div>
     </main>
   )
